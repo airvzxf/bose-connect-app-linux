859c23f19c2be25c8afad50cf7e8fe4e3c3c9238
Israel Roldan <israel.alberto.rv@gmail.com>
2021-05-17 12:51:31 -0500

    Updated the BlueZ installation with development libraries.

--- aws/setup.sh
+++ aws/setup.sh
@@ -6,14 +6,23 @@ echo "# Installation on RHEL -> Amazon Linux 2"
 echo "# ----------------------------------------------------"
 
 echo ""
-echo "# Install CMake"
+echo "# CMake3 - Install"
 echo "# ----------------------------------------------------"
 sudo yum --assumeyes install cmake3.x86_64
+echo ""
+echo "# CMake2 - Remove"
+echo "# ----------------------------------------------------"
 sudo yum --assumeyes remove cmake.x86_64
+echo ""
+echo "# CMake - List package"
+echo "# ----------------------------------------------------"
 sudo yum list cmake*
 
 echo ""
-echo "# Install BlueZ"
+echo "# BlueZ - Install"
 echo "# ----------------------------------------------------"
-sudo yum --assumeyes install bluez-libs.x86_64
+sudo yum --assumeyes install bluez-libs-devel.x86_64
+echo ""
+echo "# BlueZ - List packages"
+echo "# ----------------------------------------------------"
 sudo yum list bluez*
