b9a2d128e374ce161441cb35d852f8613dd1f0cb
Israel Roldan <israel.alberto.rv@gmail.com>
2021-05-21 15:04:43 -0500

    Restored the bluetooth.c file.

--- src/library/bluetooth.c
+++ src/library/bluetooth.c
@@ -8,8 +8,9 @@
  */
 
 int reverse_ba2str(const bdaddr_t *ba, char *str) {
-  return sprintf(str, "%2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X",
-                 ba->b[0], ba->b[1], ba->b[2], ba->b[3], ba->b[4], ba->b[5]);
+    return sprintf(str, "%2.2X:%2.2X:%2.2X:%2.2X:%2.2X:%2.2X",
+                   ba->b[0], ba->b[1], ba->b[2], ba->b[3], ba->b[4], ba->b[5]);
+
 }
 
 int reverse_str2ba(const char *str, bdaddr_t *ba) {
