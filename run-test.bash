f11b009716b06c93fe61bbdba550268a3c0d8213
Israel Roldan <israel.alberto.rv@gmail.com>
2025-07-09 07:49:58 -0600

    chore: Add cargo format to the Bash script.

--- run-test.bash
+++ run-test.bash
@@ -2,7 +2,7 @@
 # Copyright (c) 2025, Israel Alberto Roldan Vega | GPL-3.0 license
 set -euo pipefail
 
-cargo check && cargo clippy && cargo build && cargo run -- --address 2C:41:A1:02:6D:6F get-battery
+cargo fmt --all && cargo check && cargo clippy && cargo build #&& cargo run -- --address 2C:41:A1:02:6D:6F get-battery
 
 # date +'%Y-%m-%dT%H:%M:%S:%3N'
 
